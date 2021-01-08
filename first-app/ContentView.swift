e588f87cb1eab9506218454ba1dab8309fa0b0b5
Franklin Grijalva Revelo <franklingr2009@hotmail.com>
2021-01-07 21:23:48 -0500

    Initial Commit

---
+++ first-app/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  first-app
+//
+//  Created by Franklin Grijalva Revelo on 7/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .fontWeight(.bold)
+            .font(.system(.largeTitle, design: .rounded))
+            .foregroundColor(Color.red)
+            .multilineTextAlignment(.center)
+            .rotation3DEffect(.degrees(45), axis: (x: 1, y: 0, z: 0)
+                
+            )
+            .shadow(color: .gray, radius: 1, x: 0, y: 5)
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ first-app/first_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_appApp.swift
+//  first-app
+//
+//  Created by Franklin Grijalva Revelo on 7/1/21.
+//
+
+import SwiftUI
+
+@main
+struct first_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
