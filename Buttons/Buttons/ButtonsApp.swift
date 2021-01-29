978bd665ce943abe8b0076c4ecab1a7b066785fb
Franklin Grijalva Revelo <franklingr2009@hotmail.com>
2021-01-28 23:04:16 -0500

    Practica Buttons

---
+++ Buttons/Buttons/ButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonsApp.swift
+//  Buttons
+//
+//  Created by Franklin Grijalva Revelo on 28/1/21.
+//
+
+import SwiftUI
+
+@main
+struct ButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Buttons/Buttons/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Buttons
+//
+//  Created by Franklin Grijalva Revelo on 28/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
